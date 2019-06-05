2329fd6607fe128c4df583b3179f7cec52bcebd9
Gergely Gyetvai <gyetvaigergely@gmail.com>
2019-06-05 22:01:40 +0200

    Added insert_values_to_table function

--- simple_insert_to_SQLite.py
+++ simple_insert_to_SQLite.py
@@ -22,12 +22,26 @@ def connect_to_db(db_file_path):
             sqlite3_conn.close()
 
 
+def insert_values_to_table(conn, table_name):
+
+    c.execute('CREATE TABLE IF NOT EXISTS ' + table_name + '(fName TEXT, lName TEXT, title TEXT, age INT)')
+
+    values_to_insert = ('Some First Name', 'Some Last Name', 'Very good Title', 42)
+
+    sql_query = 'INSERT INTO sample_table(fName, lName, title, age) VALUES (?,?,?,?)'
+
+    c.execute(sql_query, values_to_insert)
+    conn.commit()
+
+
 if __name__ == '__main__':
-    db_file_path = 'pythonsqlite.db'
+    db_file_path = 'sampleSQLite.db'
 
     conn = connect_to_db(db_file_path)
 
     if conn is not None:
+        c = conn.cursor()
+        insert_values_to_table(conn, 'sample_table')
         conn.close()
     else:
         print('Connection to database failed')
